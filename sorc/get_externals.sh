e85e9f8b4224f93fe7c43de57ea252b6304d935c
saeideh banihashemi <saeideh.banihashemi@clogin08.cactus.wcoss2.ncep.noaa.gov>
2025-08-16 08:11:12 +0000

    adding WW3 submodule and get_external script

---
+++ sorc/get_externals.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# --------------------------------------------------------------------------- #
+#                                                                             #
+# Copy external fix files that are too large to store in repository           #
+#                                                                             #
+# Last Changed : 08-15-2025                                        Aug 2025   #
+# --------------------------------------------------------------------------- #
+
+echo 'Fetching externals...'
+cp -p /lfs/h2/emc/couple/noscrub/saeideh.banihashemi/git/fv3gfs/fix/fix_glwu/mesh.glwu ../fix/mesh.rwps
+cp -p /lfs/h2/emc/couple/noscrub/saeideh.banihashemi/git/fv3gfs/fix/fix_glwu/grint_weights.grlc_2p5km ../fix/
+cp -p /lfs/h2/emc/couple/noscrub/saeideh.banihashemi/git/fv3gfs/fix/fix_glwu/grint_weights.grlr ../fix/
+cp -p /lfs/h2/emc/couple/noscrub/saeideh.banihashemi/git/fv3gfs/fix/fix_glwu/grint_weights.grlr_500m ../fix/
